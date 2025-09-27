d487ecf7685e6f2c53b8ab62e5530dc3070d1a80
AhmedaymanEl-shamy <ahmedayman1747@gmail.com>
2025-09-27 14:35:14 +0300

    first

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -41,7 +41,7 @@ if(paymentflag == 'cash'){
       context?.handleCart()
       router.push('/allorders')
 }else{
-  const data = await makeOnlinePayment(id,'http://localhost:3000',values)
+  const data = await makeOnlinePayment(id,'https://next-ecommerce-ekvmz8fia-ahmedaymanel-shamys-projects.vercel.app/',values)
 
   console.log(data);
   if(data.status == 'success'){
